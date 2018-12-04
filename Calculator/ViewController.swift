dd08333057bfc2e67f05eeb460fb9729b4742b03
GuillaumeC <guillaume.camanes@gmail.com>
2018-12-04 13:43:15 +0100

    première version de la calculette pour iOS

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    var previousValue: Double = 0.0
+    var currentValue: String = ""
+    var operatorFunc:((Double, Double) -> (Double))?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func printData() {
+        print(previousValue)
+        print(operatorFunc)
+    }
+    
+    func resolve (newValue: Double) -> Double {
+        
+        guard let operationToExecute  = operatorFunc else {
+            let alertViewController = UIAlertController(title: "error", message: "FATAL ERROR", preferredStyle: .alert)
+            self.show(alertViewController, sender: self)
+            // Return 0 if no operation
+            return 0.0
+        }
+        // Return the result
+        return operationToExecute(previousValue, newValue)
+        printData()
+    }
 
-
+    @IBAction func digitDidTap(_ sender: UIButton) {
+        labelResult.text = (labelResult.text ?? "") + (sender.titleLabel?.text ?? "")
+        currentValue += (sender.titleLabel?.text ?? "")
+    }
+    
+    @IBAction func cancelDidTap(_ sender: UIButton) {
+        labelResult.text = ""
+        previousValue = 0.0
+        operatorFunc = nil
+        printData()
+    }
+    
+    @IBAction func dotDidTap(_ sender: UIButton) {
+        if !(labelResult.text ?? "").contains(".") {
+            labelResult.text = (labelResult.text ?? "") + "."
+        }
+        printData()
+    }
+    
+    @IBAction func operatorDidTap(_ sender: UIButton) {
+        // Getting user input value
+        guard let stringValue = labelResult.text else {
+            return
+        }
+        // Casting to Double
+        guard let value = Double(stringValue) else {
+            return
+        }
+        // Here we are sure that value is a double
+        if operatorFunc != nil {
+            previousValue = resolve(newValue: value)
+        } else {
+            previousValue = value
+        }
+        
+        printData()
+        if let ope = (sender.titleLabel?.text) {
+            if ope == "+" {
+                operatorFunc = {(a: Double, b: Double) in return a+b}
+            } else if ope == "-" {
+                operatorFunc = {(a: Double, b: Double) in return a-b}
+            } else if ope == "*" {
+                operatorFunc = {(a: Double, b: Double) in return a*b}
+            } else if ope == "/" {
+                operatorFunc = {(a: Double, b: Double) in return a/b}
+            }
+            print(ope)
+            print("\n")
+        }
+        
+        labelResult.text = ""
+    }
+    
+    @IBAction func equalDidTap(_ sender: UIButton) {
+        // Getting user input value
+        guard let stringValue = labelResult.text else {
+            return
+        }
+        // Casting to Double
+        guard let value = Double(stringValue) else {
+            return
+        }
+        
+        previousValue = resolve(newValue: value)
+        labelResult.text  = String(previousValue)
+        printData()
+    }
 }
 
